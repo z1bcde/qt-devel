82d0a1e6c3678c1a38edcbe4b90fa2fab614380a
紘史 武居 <admin@rs9101329942.onmicrosoft.com>
2020-05-26 08:51:02 +0900

    chacke correct

--- cwindow/Main.cpp
+++ cwindow/Main.cpp
@@ -1,11 +1,9 @@
-#include <QCoreApplication>
-#include <QtWidgets/QLabel>
-#include <QtGui>
-#include <QtWidgets/QMainWindow>
+#include <QApplication>
+#include <QMainWindow>
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication app(argc, argv);
+	QApplication app(argc, argv);
 	QMainWindow *main = new QMainWindow();
 	main->setGeometry(0,0,500,500);
 	main->show();

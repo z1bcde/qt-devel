f25aaa7ae87ead778533f00fd26e7f52668a1977
紘史 武居 <admin@rs9101329942.onmicrosoft.com>
2020-05-26 09:24:45 +0900

    remove makefile

--- cwindow/make.bat
+++
@@ -1,2 +0,0 @@
-g++ Main.cpp -o Main.exe -DQT_CORE_LIB -DQT_WIDGETS_LIB -DQT_GUI_LIB -IC:/Qt/5.14.2/mingw73_64/include/QtCore -IC:/Qt/5.14.2/mingw73_64/include -IC:/Qt/5.14.2/mingw73_64/include/QtWidgets -IC:/Qt/5.14.2/mingw73_64/include/QtGui -IC:/Qt/5.14.2/mingw73_64/include/QtANGLE  -LC:/Qt/5.14.2/mingw73_64/lib -lQt5Widgets -lQt5Gui -lQt5Core  
-
--- cwindow/make.sh
+++
@@ -1,1 +0,0 @@
-g++ -w Main.cpp -o Main-x64-linux -fPIC $(pkg-config --cflags --libs Qt5Core Qt5Widgets)
